e2cb2b7b489cb2accfe6d57d5f47f46e7eec2fac
Brandon Craig <twilightraven13@yahoo.com>
2020-12-23 04:16:20 -0800

    Created the framework for the movie response in script.js. Need divs and elements to hold the response. Comments are included on the index.html.

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 $("#searchBtn").on("click", function() {
     if($("#searchBar").val() === "") {
+        //Don't use alert
         alert("Enter a movie title");
         return;
     }
     
     var searchVal = $("#searchBar").val();
     var key = "8549cdbb";
-    var movieQuery = "https://www.omdbapi.com/?s=" + searchVal + "&y=&plot=short&apikey=" + key;
+    var movieQuery = "https://www.omdbapi.com/?t=" + searchVal + "&y=&plot=short&apikey=" + key;
+    
 
     $.ajax({
-        url: queryURL,
+        url: movieQuery,
         method: "GET",
       }).then(function (response) {
+        // The movie response works. Just need divs and placements on index
         console.log(response);
-        
+        var movieTitle = response.Title;
+        //Select element-.text(movieTitle);
+        var poster = response.Poster;
+        //$("<img>").attr("src", poster) <img> will need an id
+        var releaseYear = response.Released;
+        //Select element-.text(releaseYear)
+        var rating = response.Rated;
+        //select element-.text(rating);
+        var actors = response.Actors;
+        //select element-.text(actors);
+        var plot = response.Plot;
+        //select element-.text(plot);
+        //Do not use append(). .text will set the text of the span/element
+
       });
 })
+
